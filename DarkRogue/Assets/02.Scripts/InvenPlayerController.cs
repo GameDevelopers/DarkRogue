063fe99a930bae025413b26dae9deffaf50e954a
kyeongseoJ <86696753+kyeongseoJ@users.noreply.github.com>
2022-04-20 19:21:23 +0900

    Create Project
    레포 융합 시작

---
+++ DarkRogue/Assets/02.Scripts/InvenPlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvenPlayerController : MonoBehaviour
+{
+
+    // 이동위한 벡터 변수
+    float moveX, moveY;
+
+    public bool talk = false;
+    public string[] sentences;
+    float lastSpaceTime = 0f;
+    float spaceTime = 0.5f;
+
+    // 이동속도 
+    [Header("이동속도")]
+    // 이동 속도 범위 제한 조절
+    [SerializeField] [Range(1f, 30f)] float moveSpeed = 6f;
+
+    // 캐릭터 이동
+    private void Update()
+    {
+        PlayerMove();
+        if(talk) NPCTalk();
+
+    }
+
+    void PlayerMove()
+    {
+        moveY = Input.GetAxis("Vertical") * moveSpeed;
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+
+        transform.position += new Vector3(moveX, moveY, 0) * Time.deltaTime;
+    }
+
+    void NPCTalk()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= lastSpaceTime + spaceTime)
+        {
+            lastSpaceTime = Time.time;
+            //if (DialogManager.instance.dialoggroup.alpha == 0 && !DialogManager.instance.istyping)
+            //{
+            //    DialogManager.instance.Ondialog(sentences);
+            //}
+            //else if (DialogManager.instance.dialoggroup.alpha == 1 && DialogManager.instance.sentences.Count != 0)
+            //{
+            //    DialogManager.instance.NextSentence();
+            //    //if (DialogManager.instance.istyping) DialogManager.instance.jump = true;
+            //    //DialogManager.instance.jump = true;
+            //}
+            switch (DialogManager.instance.dialoggroup.alpha)
+            {
+                case 0:
+                    if (!DialogManager.instance.istyping)
+                    {
+                        DialogManager.instance.Ondialog(sentences);
+                    }
+                    break;
+                case 1:
+                    if(DialogManager.instance.sentences.Count != 0)
+                    {
+                        DialogManager.instance.NextSentence();
+                    }
+                    else
+                    {
+                        if (DialogManager.instance.jump) DialogManager.instance.NextSentence();
+                        else DialogManager.instance.jump = true;
+                        //DialogManager.instance.jump = true;
+                    }
+                    break;
+                default:
+                    Debug.Log("Error");
+                    break;
+            }
+        }
+    }
+
+
+}
---
+++ DarkRogue/Assets/02.Scripts/NPCtalk.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCtalk : MonoBehaviour
+{
+
+    public string[] sentences;
+
+    private void OnMouseDown()
+    {
+        if(DialogManager.instance.dialoggroup.alpha == 0)
+        {
+            DialogManager.instance.Ondialog(sentences);
+        }
+    }
+
+    private void Update()
+    {
+        
+        
+            //if (Input.GetKeyDown(KeyCode.Space)&& gameObject.CompareTag("Player"))
+            //{
+            //    if(DialogManager.instance.dialoggroup.alpha == 0)
+            //    {
+            //        DialogManager.instance.Ondialog(sentences);
+            //    }
+            //}
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            InvenPlayerController playerController = collision.gameObject.GetComponent<InvenPlayerController>();
+            playerController.talk = true;
+            playerController.sentences = this.sentences;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") collision.gameObject.GetComponent<InvenPlayerController>().talk = false;
+    }
+}
