3107c7d5529f8c7ead5908e7cb18a20acd7d88f2
kyeongseoJ <86696753+kyeongseoJ@users.noreply.github.com>
2022-04-28 17:53:15 +0900

    update

--- DarkRogue/Assets/03.Scripts/invenshopUI/GameManager.cs
+++ DarkRogue/Assets/03.Scripts/invenshopUI/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject menuPanel; // 메뉴 클릭시 패널 활성화
+    public bool isPause = false; // 일시정지 상태
+
     public void Startgame()
     {
         if (Input.GetMouseButtonDown(0))
@@ -15,7 +18,7 @@ public class GameManager : MonoBehaviour
 
     public void HomeMenu()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene(0);
         }
@@ -23,11 +26,33 @@ public class GameManager : MonoBehaviour
 
     public void Exitgame()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             Application.Quit();
         }
     }
+
+    // 메뉴 클릭시 화면 일시정지 및 패널 활성화
+    public void OnMenu()
+    {
+        if (!isPause)
+        {   // 일시정지 중이 아니면 일시정지
+            Time.timeScale = 0; // 시간정지
+            // 패널활성화
+            menuPanel.SetActive(true);
+            Debug.Log("click");
+        }
+        else
+        {
+            Time.timeScale = 1.0f; // 시간흐름 비율 1
+            menuPanel.SetActive(false);
+        }
+
+        isPause = !isPause; // 메뉴 누를 때마다 상태가 반대로 바뀜
+    }
+
+
+
 
 
 }
