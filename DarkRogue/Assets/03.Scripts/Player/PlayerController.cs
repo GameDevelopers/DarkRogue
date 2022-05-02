449c3ec1f8c2981e0a83c3ac2a8413d0960bf261
kyeongseoJ <86696753+kyeongseoJ@users.noreply.github.com>
2022-05-02 18:19:52 +0900

    player TT

--- DarkRogue/Assets/03.Scripts/Player/PlayerController.cs
+++ DarkRogue/Assets/03.Scripts/Player/PlayerController.cs
@@ -224,13 +224,14 @@ public class PlayerController : MonoBehaviour
                 newScale.y = 1;
                 newScale.z = 1;
                 PlaySound("MOVE");
-
+                Debug.Log("move");
                 // 플레이어의 크기를 위에서 설정한 것으로 초기화.
                 playerTransform.localScale = newScale;
 
                 // 만약에 땅에 닿았다면.
                 if (isGround)
                 {
+                    Debug.Log("isground");
                     // 되돌리는 애니매이션 on.
                     animator.SetTrigger("IsRotate");
                 }
@@ -238,6 +239,7 @@ public class PlayerController : MonoBehaviour
             // 또 만약에 이동방향이 0보다 크다면(오
             else if (moveDirection > 0)
             {
+                Debug.Log("isrun");
                 // 달리는 애니메이션 on.
                 animator.SetBool("IsRun", true);
             }
@@ -535,7 +537,7 @@ public class PlayerController : MonoBehaviour
         float radius = 0.5f;
 
         float distance = -3f;
-        LayerMask layerMask = LayerMask.GetMask("Enemy") | LayerMask.GetMask("Boss");
+        LayerMask layerMask = LayerMask.GetMask("Enemy") | LayerMask.GetMask("Boss") | LayerMask.GetMask("Door"); ;
         Debug.DrawRay(origin, detectDirection, Color.red, 1f);
         RaycastHit2D[] hitRecList = Physics2D.CircleCastAll(origin, radius, detectDirection, distance, layerMask);
 
@@ -558,6 +560,11 @@ public class PlayerController : MonoBehaviour
                 if (boss != null)
                     boss.BossDamaged(1);
 
+            }
+            else if (layerName == "Door")
+            {
+                DoorCrush door = obj.GetComponent<DoorCrush>();
+                door.HitDoor(1);
             }
 
         }
